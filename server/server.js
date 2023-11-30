4a4762f602e77ebc724640328fc3b1e7756ceabb
yuvalzh <yuvi.zehavi@gmail.com>
2023-11-30 18:01:42 +0200

    server init

--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
-// server/index.js
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import routes from "./routes/index.js";
+// import databaseConnect from "./database/index.js";
 
-const express = require("express");
-
-const PORT = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+// connect to database
+// databaseConnect();
+
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// app.use("/api", routes);
+
+app.listen(port, () => console.log(`Server Running on Port:${port}`));
